4d34e0bb9100a6caac14043432aca9738279dd64
saloni1108 <saloni1108goyal@gmail.com>
2024-05-08 12:13:02 +0530

    [Saloni]: A logger file

---
+++ loggers.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(log_file):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    return logger
